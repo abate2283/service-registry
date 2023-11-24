d414d46c9836fba051bf1abefc71da54fbc827d6
abate2283 <alfredbate@yahoo.co.uk>
2023-11-24 07:23:21 -0600

    Initial commit

---
+++ src/test/java/com/dailycodebuilder/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuilder.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
